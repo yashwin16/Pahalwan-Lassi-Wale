d10fc114819c443f19ac33eb883221ed15282720
Yashwin Sharma <yashwinsharma16@gmail.com>
2026-08-26 23:41:52 +0530

    add resend form

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from 'next';
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.pahalwanlassiwaleandsweets.com';
+  const baseUrl = 'https://pahalwanlassiwaleandsweets.com';
 
   return [
     {
