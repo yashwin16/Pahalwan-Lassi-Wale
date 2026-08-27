49ff6f8c559c4eb69e4250da6953b2eec03263c6
Yashwin Sharma <yashwinsharma16@gmail.com>
2026-08-27 10:50:39 +0530

    fix icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,17 +31,6 @@ export const metadata: Metadata = {
     locale: "en_IN",
     type: "website",
   },
-  icons: {
-    icon: [
-      {
-        url: "/icon.png",
-        type: "image/png",
-        sizes: "512x512",
-      },
-    ],
-    shortcut: "/favicon.ico",
-    apple: "/icon.png",
-  },
 };
 
 const structuredData = {
